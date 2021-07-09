0ee719c329d882416cb9197491139a92e5777a83
Roberto Meneghelli <roberto.mng.89@gmail.com>
2021-07-08 21:41:16 -0300

    Metodo HasColumnType para declarar o tipo de campo no banco de dados
    Commit (Metodo HasColumnType para declarar o tipo de campo no banco de dados)

---
+++ DefaultAPI.Domain/Entities/Notification.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefaultAPI.Domain.Entities
+{
+    public class Notification
+    {
+        public long? Id { get; set; }
+        public string Description { get; set; }
+        public DateTime? CreatedTime { get; set; }
+        public bool IsActive { get; set; }
+
+        public Notification()
+        {
+
+        }
+
+        public Notification(long? id, string description, DateTime createdTime, bool isActive)
+        {
+            this.Id = id;
+            this.Description = description;
+            this.CreatedTime = createdTime;
+            this.IsActive = isActive;
+        }
+
+        public Notification(string description, DateTime createdTime, bool isActive)
+        {
+            this.Description = description;
+            this.CreatedTime = createdTime;
+            this.IsActive = isActive;
+        }
+    }
+}
