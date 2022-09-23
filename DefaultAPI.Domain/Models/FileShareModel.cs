987ad6971d4457bbea319c6fa28d2bfa5ad3bb49
Roberto Meneghelli <roberto.mng.89@gmail.com>
2022-09-23 16:16:50 -0300

    Versão 4.9 (Download de arquivos armazenados no azure)
    Commit (Versão 4.9 (Download de arquivos armazenados no azure))

---
+++ DefaultAPI.Domain/Models/FileShareModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace DefaultAPI.Domain.Models
+{
+    public class FileShareModel
+    {
+        public bool IsSuccess { get; set; } = false;
+        public string FileName { get; set; } = string.Empty;
+        public string FileExtension { get; set; } = string.Empty;
+        public string DataBase64 { get; set; } = string.Empty;
+
+        public FileShareModel()
+        {
+
+        }
+
+        public FileShareModel(string fileName, string fileExtension, Stream streamFile)
+        {
+            if (streamFile != null)
+            {
+                IsSuccess = true;
+                FileName = fileName;
+                FileExtension = fileExtension;
+                DataBase64 = GetBase64FromStream(streamFile);
+            }
+        }
+
+        private string GetBase64FromStream(Stream streamFile)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                streamFile.CopyTo(ms);
+                return Convert.ToBase64String(ms.ToArray());
+            }
+        }
+    }
+}
---
+++ DefaultAPI.Infra.CrossCutting/FileSharedAzureService.cs
@@ -0,0 +1,56 @@
+﻿using DefaultAPI.Domain.Models;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.File;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefaultAPI.Infra.CrossCutting
+{
+    public static class FileSharedAzureService
+    {
+        private static CloudFileClient GetStorageClientFile()
+        {
+            string accountName = "Azure-FileShare-AccountName";
+            string keyValue = "Azure-FileShare-KeyValue";
+
+            CloudStorageAccount storageAccount =
+              new CloudStorageAccount(
+                new StorageCredentials(accountName, keyValue), true);
+
+            return storageAccount.CreateCloudFileClient();
+        }
+
+        /// <summary>
+        /// Metodo respónsavel pelo download de um documento armazenado no file share do Azure
+        /// </summary>
+        /// <param name="fileName">Nome completo do arquivo. ex: arquivo.pdf</param>
+        /// <param name="fileExtension">Sua extensão somente. ex: .pdf</param>
+        /// <param name="shareReference">Pasta em que o arquivo se encontra</param>
+        /// <returns></returns>
+        public static async Task<FileShareModel> DownloadFile(string fileName, string fileExtension = ".pdf", string shareReference = "documentFiles")
+        {
+            try
+            {
+                CloudFileShare fileShare = GetStorageClientFile().GetShareReference(shareReference);
+
+                if (await fileShare.ExistsAsync())
+                {
+                    CloudFileDirectory rootDir = fileShare.GetRootDirectoryReference();
+                    CloudFile file = rootDir.GetFileReference(fileName);
+                    var ms = new MemoryStream();
+                    return new FileShareModel(fileName, fileExtension, await file.OpenReadAsync());
+                }
+
+                return null;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message, ex.InnerException);
+            }
+        }
+    }
+}
